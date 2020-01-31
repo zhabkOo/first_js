fb3f630459a4884c9b002742c7e3fb6b8d153eac
zhabkOo <freem.ua@gmail.com>
2020-01-31 17:39:03 +0200

    Bug fixing of 0 =  in third script. Edit comments

--- third_script.js
+++ third_script.js
@@ -1,30 +1,54 @@
 //2.3----------------GUESS NUMBER GAME------------------//
-let ageCheck = confirm("Do you have a 18?");
-let randomNumber = Math.round(Math.random() * 10);
-let userNumber = undefined;
+let ageCheck = confirm("Do you have a 18?"); //Confirm: OK/CANCEL
+let randomNumber = Math.round(Math.random() * 10); //Round a number to the nearest integer
+let userNumber = undefined; //empty variable
 
 console.log("randomNumber:", randomNumber);
+
 if (ageCheck === true) {
   userNumber = prompt("Guess the number from 0 to 10");
   console.log("userNumber:", userNumber, "randomNumber:", randomNumber);
-  if (+userNumber === randomNumber) {
-    alert("Congratulate, you win!");
-  } else if (userNumber === null) {
-    alert("You refused to play a game with me"); //check pressing cancel button
-  } else if (userNumber === "") {
+  if (userNumber === "") {
     alert("Give me more data"); //check pressing OK button with empty input
+  } else if (userNumber === null) {
+    alert("You refused to play a game with me"); //check pressing CANCEL button
+  } else if (+userNumber === randomNumber) {
+    alert("Congratulate, you win!"); //IF USER WON GAME
   } else if (isNaN(userNumber)) {
-    alert("I understand only numbers");
+    alert("I understand only numbers"); //Abracadabra check :)
   } else if (userNumber < 0 || userNumber > 10) {
-    alert("Your number is out of range from 0 to 10");
-  } else if (userNumber != randomNumber) {
-    alert("You lose! :(");
+    alert("Your number is out of range from 0 to 10"); //check Out Of Range
+  } else {
+    alert("You lose! :("); //IF USER LOSE GAME
   }
 } else {
   console.log("ageCheck:", ageCheck);
   alert("Sorry, you cannot play this game.");
   window.close();
 }
+
+// if (ageCheck === true) {
+//   userNumber = prompt("Guess the number from 0 to 10");
+//   console.log("userNumber:", userNumber, "randomNumber:", randomNumber);
+//   if (+userNumber === randomNumber) {
+//     alert("Congratulate, you win!"); //IF USER WON GAME
+//   } else if (userNumber === null) {
+//     alert("You refused to play a game with me"); //check pressing CANCEL button
+//   } else if (userNumber === "") {
+//     console.log("userNumber if empty OK:", userNumber);
+//     alert("Give me more data"); //check pressing OK button with empty input
+//   } else if (isNaN(userNumber)) {
+//     alert("I understand only numbers");
+//   } else if (userNumber < 0 || userNumber > 10) {
+//     alert("Your number is out of range from 0 to 10"); //check Out Of Range
+//   } else if (userNumber != randomNumber) {
+//     alert("You lose! :("); //IF USER LOSE GAME
+//   }
+// } else {
+//   console.log("ageCheck:", ageCheck);
+//   alert("Sorry, you cannot play this game.");
+//   window.close();
+// }
 
 // if (userNumber === randomNumber) {
 //   alert("Congratulate, you win!");
