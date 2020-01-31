be8e107944e349cf6795557482ad48b325bd48a2
zhabkOo <freem.ua@gmail.com>
2020-01-31 17:01:07 +0200

    third script finished

--- third_script.js
+++ third_script.js
@@ -1,27 +1,46 @@
 //2.3----------------GUESS NUMBER GAME------------------//
 let ageCheck = confirm("Do you have a 18?");
 let randomNumber = Math.round(Math.random() * 10);
-let userNumber = +"";
+let userNumber = undefined;
 
-console.log(randomNumber);
+console.log("randomNumber:", randomNumber);
 if (ageCheck === true) {
   userNumber = prompt("Guess the number from 0 to 10");
+  console.log("userNumber:", userNumber, "randomNumber:", randomNumber);
+  if (+userNumber === randomNumber) {
+    alert("Congratulate, you win!");
+  } else if (userNumber === null) {
+    alert("You refused to play a game with me"); //check pressing cancel button
+  } else if (userNumber === "") {
+    alert("Give me more data"); //check pressing OK button with empty input
+  } else if (isNaN(userNumber)) {
+    alert("I understand only numbers");
+  } else if (userNumber < 0 || userNumber > 10) {
+    alert("Your number is out of range from 0 to 10");
+  } else if (userNumber != randomNumber) {
+    alert("You lose! :(");
+  }
 } else {
+  console.log("ageCheck:", ageCheck);
   alert("Sorry, you cannot play this game.");
   window.close();
 }
-if (userNumber === randomNumber) {
-  alert("Congratulate, you win!");
-} else if (userNumber === null) {
-  alert("You refused to play a game with me"); //check pressing cancel button
-} else if (userNumber === "") {
-  alert("Give me more data"); //check pressing OK button with empty input
-} else if (isNaN(userNumber)) {
-  alert("I understand only numbers");
-} else if (userNumber < 0 || userNumber > 10) {
-  alert("Your number is out of range from 0 to 10");
-} else {
-  alert("You lose! :(");
-}
-console.log(ageCheck);
-console.log(randomNumber);
+
+// if (userNumber === randomNumber) {
+//   alert("Congratulate, you win!");
+// } else if (userNumber === null) {
+//   alert("You refused to play a game with me"); //check pressing cancel button
+// } else if (userNumber === "") {
+//   alert("Give me more data"); //check pressing OK button with empty input
+// } else if (isNaN(userNumber)) {
+//   alert("I understand only numbers");
+// } else if (userNumber < 0 || userNumber > 10) {
+//   alert("Your number is out of range from 0 to 10");
+// } else if (userNumber != randomNumber && userNumber != false) {
+//   alert("You lose! :(");
+// }
+// console.log(ageCheck);
+// console.log(randomNumber);
+// //   // alert("Sorry, you cannot play this game.");
+// //   // window.close();
+// // }
